1fd815f775f48423b52e5ea5d4fe747657865da4
gabriielf54 <gabriel.nuno22@gmail.com>
2024-05-08 21:28:16 -0300

    criar exceptions

---
+++ financeiro-api/src/main/java/com/gabriel/financeiroapi/exception/ErroAutenticacaoException.java
@@ -0,0 +1,8 @@
+package com.gabriel.financeiroapi.exception;
+
+public class ErroAutenticacaoException extends RuntimeException {
+
+    public ErroAutenticacaoException(String msg) {
+        super(msg);
+    }
+}
