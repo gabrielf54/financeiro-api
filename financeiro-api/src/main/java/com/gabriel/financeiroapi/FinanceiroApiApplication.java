a0beb5975382a4774f3081392d0e74d785df6482
gabriielf54 <gabriel.nuno22@gmail.com>
2024-06-25 21:27:26 -0300

    autorizar requisicoes feitas no frontend

--- financeiro-api/src/main/java/com/gabriel/financeiroapi/FinanceiroApiApplication.java
+++ financeiro-api/src/main/java/com/gabriel/financeiroapi/FinanceiroApiApplication.java
@@ -2,9 +2,18 @@ package com.gabriel.financeiroapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FinanceiroApiApplication {
+@EnableWebMvc
+public class FinanceiroApiApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceiroApiApplication.class, args);
